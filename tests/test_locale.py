46646f678ca2c5000a5d5849664cfda19d707e4f
Anton <antonio.maksimovich@gmail.com>
2019-07-04 17:44:22 +0300

    Expand tests

--- tests/test_locale.py
+++ tests/test_locale.py
@@ -6,12 +6,24 @@ async def test_for_existing_user(client):
     assert "ru" in data["locale"]
 
 
+async def test_for_nonexistent_locale(client):
+    resp = await client.get("/locale?user_id=rocket.dog")
+    assert resp.status == 200
+    data = await resp.json()
+    assert "" in data["locale"]
+
+
 async def test_for_nonexistent_user(client):
     resp = await client.get("/locale?user_id=qwerty")
     assert resp.status == 404
 
 
 async def test_for_bad_request(client):
+    resp = await client.get("/locale")
+    assert resp.status == 400
+    text = await resp.text()
+    assert "user_id not found" in text
+
     resp = await client.get("/locale?test=qwerty")
     assert resp.status == 400
     text = await resp.text()
