895b73cdee22a8fbd9f14a8afeacbed7e927c4f9
Anton <antonio.maksimovich@gmail.com>
2019-07-04 16:22:14 +0300

    Fix default mongo port

--- src/settings.py
+++ src/settings.py
@@ -6,4 +6,4 @@ PORT = int(os.getenv('PORT') or '8000')
 HOST = os.getenv('HOST') or 'localhost'
 
 MONGODB_HOST = os.getenv('MONGODB_HOST') or 'localhost'
-MONGODB_PORT = int(os.getenv('MONGODB_PORT') or '5432')
+MONGODB_PORT = int(os.getenv('MONGODB_PORT') or '27017')
